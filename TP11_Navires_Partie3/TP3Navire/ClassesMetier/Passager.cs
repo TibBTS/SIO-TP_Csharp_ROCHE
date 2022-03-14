d0b9cb53cd9868a31fcd012c55747b2afd8ecb74
Tibo <95851801+Tibo97@users.noreply.github.com>
2022-03-14 10:59:43 +0100

    Update Passager.cs

--- TP11_Navires_Partie3/TP3Navire/ClassesMetier/Passager.cs
+++ TP11_Navires_Partie3/TP3Navire/ClassesMetier/Passager.cs
@@ -12,5 +12,20 @@ namespace NavireHeritage.ClassesMetier
         private string nom;
         private string prenom;
         private string nationalité;
+        
+        public string NumPasseport { get => numPasseport; set => numPasseport = value; }
+        public string Nom { get => nom; set => nom = value; }
+        public string Prenom { get => prenom; set => prenom = value; }
+        public string Nationalité { get => nationalité; set => nationalité = value; }
+
+        public Passager(string numPasseport, string nom, string prenom, string nationalité)
+        {
+            this.numPasseport = numPasseport;
+            this.nom = nom;
+            this.prenom = prenom;
+            this.nationalité = nationalité;
+        }
+
+        
     }
 }
