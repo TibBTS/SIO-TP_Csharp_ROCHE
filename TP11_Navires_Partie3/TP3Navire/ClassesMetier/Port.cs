a221eee09b3748dfb881d2cace93838d7d484cbc
Tibo <95851801+Tibo97@users.noreply.github.com>
2022-03-17 15:09:23 +0100

    Update Port.cs

--- TP11_Navires_Partie3/TP3Navire/ClassesMetier/Port.cs
+++ TP11_Navires_Partie3/TP3Navire/ClassesMetier/Port.cs
@@ -15,9 +15,8 @@ namespace NavireHeritage.ClassesMetier
         private int nbQuaisTanker;
         private int nbQuaisSuperTanker;
         private int nbQuaisPassager;
-
-
-
+        
+        
         public string Nom { get => nom; }
         public string Latitude { get => latitude; }
         public string Longitude { get => longitude; }
@@ -25,6 +24,20 @@ namespace NavireHeritage.ClassesMetier
         public int NbQuaisTanker { get => nbQuaisTanker; set => nbQuaisTanker = value; }
         public int NbQuaisSuperTanker { get => nbQuaisSuperTanker; set => nbQuaisSuperTanker = value; }
         public int NbQuaisPassager { get => nbQuaisPassager; set => nbQuaisPassager = value; }
+        
+        
+        public Port(string nom, string latitude, string longitude, int nbPortique, int nbQuaisTanker, int nbQuaisSuperTanker, int nbQuaisPassager)
+        {
+            this.nom = nom;
+            this.latitude = latitude;
+            this.longitude = longitude;
+            this.nbPortique = nbPortique;
+            this.nbQuaisTanker = nbQuaisTanker;
+            this.nbQuaisSuperTanker = nbQuaisSuperTanker;
+            this.nbQuaisPassager = nbQuaisPassager;
+        }
+
+        
 
 
 
