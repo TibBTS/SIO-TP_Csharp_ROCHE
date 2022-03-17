afefa92fa0a9f7a108d16112094cd6e1d09fcd24
Tibo <95851801+Tibo97@users.noreply.github.com>
2022-03-17 15:55:57 +0100

    Update Port.cs

--- TP11_Navires_Partie3/TP3Navire/ClassesMetier/Port.cs
+++ TP11_Navires_Partie3/TP3Navire/ClassesMetier/Port.cs
@@ -15,32 +15,53 @@ namespace NavireHeritage.ClassesMetier
         private int nbQuaisTanker;
         private int nbQuaisSuperTanker;
         private int nbQuaisPassager;
-        
-        
-        public string Nom { get => nom; }
-        public string Latitude { get => latitude; }
-        public string Longitude { get => longitude; }
-        public int NbPortique { get => nbPortique; set => nbPortique = value; }
-        public int NbQuaisTanker { get => nbQuaisTanker; set => nbQuaisTanker = value; }
-        public int NbQuaisSuperTanker { get => nbQuaisSuperTanker; set => nbQuaisSuperTanker = value; }
-        public int NbQuaisPassager { get => nbQuaisPassager; set => nbQuaisPassager = value; }
-        
-        
+        private Dictionary<String, Navire> navireAttendus = new Dictionary<string, Navire>();
+        private Dictionary<String, Navire> navireArrives = new Dictionary<string, Navire>();
+        private Dictionary<String, Navire> navirePartis = new Dictionary<string, Navire>();
+        private Dictionary<String, Navire> navireEnAttente = new Dictionary<string, Navire>();
+
         public Port(string nom, string latitude, string longitude, int nbPortique, int nbQuaisTanker, int nbQuaisSuperTanker, int nbQuaisPassager)
         {
             this.nom = nom;
             this.latitude = latitude;
             this.longitude = longitude;
-            this.nbPortique = nbPortique;
-            this.nbQuaisTanker = nbQuaisTanker;
-            this.nbQuaisSuperTanker = nbQuaisSuperTanker;
-            this.nbQuaisPassager = nbQuaisPassager;
+            this.NbPortique = nbPortique;
+            this.NbQuaisTanker = nbQuaisTanker;
+            this.NbQuaisSuperTanker = nbQuaisSuperTanker;
+            this.NbQuaisPassager = nbQuaisPassager;
         }
 
+        public string Nom { get => nom;  }
+        public string Latitude { get => latitude;  }
+        public string Longitude { get => longitude; }
+        public int NbPortique { get => nbPortique; set => nbPortique = value; }
+        public int NbQuaisTanker { get => nbQuaisTanker; set => nbQuaisTanker = value; }
+        public int NbQuaisSuperTanker { get => nbQuaisSuperTanker; set => nbQuaisSuperTanker = value; }
+        public int NbQuaisPassager { get => nbQuaisPassager; set => nbQuaisPassager = value; }
+        internal Dictionary<string, Navire> NavireAttendus { get => navireAttendus; }
+        internal Dictionary<string, Navire> NavireArrives { get => navireArrives; }
+        internal Dictionary<string, Navire> NavirePartis { get => navirePartis;  }
+        internal Dictionary<string, Navire> NavireEnAttente { get => navireEnAttente; }
+    }
 
-        
-
-
+    public void enregistrerArriveePrevue(Object)
+    {
 
     }
+
+    public void EnregistrerArrivee(Object)
+    {
+
+    }
+
+    public void EnregistrerDepart(Object)
+    {
+    }
+
+    public bool EstAttendu(String)
+    {
+
+    }
+
+    public EstPresent(String)
 }
